9d5c6eb75b19e0ab0a2cbbc668d11102d53044ef
ScoobyJoo <joowanglee8@gmail.com>
2026-08-15 22:11:14 -0700

    Opened port 22

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -8,11 +8,11 @@ resource "aws_security_group" "app" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "SSH - restricted to a trusted IP only"
+    description = "SSH - Only accessible with private key"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.allowed_ssh_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
